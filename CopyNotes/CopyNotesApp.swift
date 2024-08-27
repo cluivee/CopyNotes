f65100716852e0d3f909c3e1bbe18271111af637
Clive <cluivee@gmail.com>
2024-08-27 22:56:22 +0100

    Initial Commit

---
+++ CopyNotes/CopyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyNotesApp.swift
+//  CopyNotes
+//
+//  Created by Clive on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CopyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
