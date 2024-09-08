c632e6ff1675a3ff267e8958a5d2ba5c5a5359e5
Clive <cluivee@gmail.com>
2024-09-08 12:02:48 +0100

    Testing coredata

--- CopyNotesTests/CopyNotesTests.swift
+++ CopyNotesTests/CopyNotesTests.swift
@@ -13,27 +13,30 @@ class CopyNotesTests: XCTestCase {
     
     var persistenceController: PersistenceController!
     var context: NSManagedObjectContext!
+    var backgroundContext: NSManagedObjectContext!
     
     override func setUpWithError() throws {
         // Creating an in-memory persistence container to avoid using the actual database. Otherwise, tests couldn't be considered isolated since one test may overwrite the contents of another test. Also the tests aren't repeatable if the data is saved to disk, as the data in the database might grow over time and the state of the environment might be different on each test run.
         
         persistenceController = PersistenceController(inMemory: true)
         context = persistenceController.container.viewContext
+        backgroundContext = persistenceController.container.newBackgroundContext()
+        
     }
     
     override func tearDownWithError() throws {
         persistenceController = nil
         context = nil
+        backgroundContext = nil
     }
     
-    func testCreateNote() throws {
+    func testAddNote() throws {
         
         let note = Note(title: "Testing Note", bodyText: "Test Body", context: context)
         
-        // Save the note
         persistenceController.save()
         
-        // Fetch the note to verify it was created
+        // Fetching the note to verify it was created
         let fetchRequest: NSFetchRequest<Note> = Note.fetch()
         
         let notes = try context.fetch(fetchRequest)
@@ -41,6 +44,7 @@ class CopyNotesTests: XCTestCase {
         XCTAssertEqual(notes.count, 1, "1 note in database")
         XCTAssertEqual(notes.first?.title, "Testing Note", "The title should match")
         XCTAssertEqual(notes.first?.bodyText, "Test Body", "The body text should match")
+        XCTAssertNotNil(notes.first?.id, "id should have a value")
     }
     
     func testDeleteNote() throws {
@@ -63,29 +67,66 @@ class CopyNotesTests: XCTestCase {
         XCTAssertEqual(notes.count, 0, "Should have 0 notes after deletion")
     }
     
+    
+    func testRootContextIsSavedAfterAddingReport() {
+        
+        // Create an expectation for a background context save notification
+        let expectation = self.expectation(description: "Background context save")
+        
+        // add observer for that notification
+        NotificationCenter.default.addObserver(forName: .NSManagedObjectContextDidSave, object: backgroundContext, queue: .main) { _ in
+            expectation.fulfill()
+        }
+        
+        // Adding a new note in the background context. Perform asynchoronously performs the closure on the context's queue
+        backgroundContext.perform {
+            let newNote = Note(context: self.backgroundContext)
+            newNote.title = "Test Title"
+            newNote.bodyText = "Test note body"
+            do {
+                try self.backgroundContext.save()
+            } catch {
+                XCTFail("Failed to save background context: \(error)")
+            }
+        }
+        
+        // Wait for the expectation to be fulfilled or timeout after 2 seconds
+        waitForExpectations(timeout: 2) { error in
+            XCTAssertNil(error, "Save did not occur")
+        }
+    }
+    
+    
     func testFetchNotes() throws {
         // example to test
-        for i in 1...3 {
-            _ = Note(title: "Note \(i)", bodyText: "Body \(i)", context: context)
-        }
+        let note = Note(title: "To Delete", bodyText: "This note will be deleted", context: context)
         persistenceController.save()
         
         let fetchRequest: NSFetchRequest<Note> = Note.fetch()
         let notes = try context.fetch(fetchRequest)
         
+        XCTAssertNotNil(notes)
         XCTAssertEqual(notes.count, 3, "Should have 3 notes fetched")
     }
     
-    func testSavePerformance() throws {
-        measure {
-            // Measure time taken to create and save a note
-            for _ in 0..<100 {
-                let note = Note(title: "Performance Test Note", bodyText: "Body", context: context)
-                context.insert(note)
-            }
-            persistenceController.save()
-        }
+    
+    func testUpdateNotes() throws {
+        let updatingTestNote = Note(title: "Initial title", bodyText: "Initial Text",  context: context)
+        
+        updatingTestNote.title = "Updated title"
+        updatingTestNote.bodyText = "Updated text"
+        
+        persistenceController.save()
+        
+        let fetchRequest: NSFetchRequest<Note> = Note.fetch()
+        let notes = try context.fetch(fetchRequest)
+        
+        // Asserting that the new note id maches the old one
+        XCTAssertTrue(notes.first?.id == updatingTestNote.id)
+        XCTAssertTrue(notes.first?.title == "Updated title")
+        XCTAssertTrue(notes.first?.bodyText == "Updated text")
     }
     
-
+    
+    
 }
